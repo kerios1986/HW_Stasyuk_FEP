45340eb19275d6ac750782c7cdb8eb2e01cceede
Sergey Stasyuk <kerrios1986@gmail.com>
2021-04-15 17:48:11 +0300

    HW-02. Dobavil sravnenie na odinakovue chisla 3zadacha

--- HW-02/3zadacha/main.js
+++ HW-02/3zadacha/main.js
@@ -3,12 +3,16 @@ b = +prompt('Введите число b');
 c = +prompt('Введите число c');
 
 
+
 if (a > b && a > c) {
     document.write('Число a = ', a, ' максимальное');
 
 } else if (b > c && b > a) {
     document.write('Число b = ', b, ' максимальное');
 
+} else if (c > b && c > a) {
+    document.write('Число b = ', c, ' максимальное');
+
 } else {
-    document.write('Число с = ', c, ' максимальное');
+    document.write('Все числа равны друг другу и одинаковые');
 }
